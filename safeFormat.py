7fed1e86ccc1bf23bd6e88d7c902bef1635b14ce
June-Skeeter <thruhikerflash@gmail.com>
2025-04-01 11:59:54 -0700

    sixth

--- safeFormat.py
+++ safeFormat.py
@@ -1,4 +1,4 @@
 # replace all non-alphanumeric characters with a safe value
 import re
-def safeFmt(string,safeCharacters='[^0-9a-zA-Z]+',safeFill='_'):
+def safeFormat(string,safeCharacters='[^0-9a-zA-Z]+',safeFill='_'):
     return(re.sub(safeCharacters,safeFill, str(string)))
