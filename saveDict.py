5f4898d42126ef828bb85aecbdb8de4a3da6ad68
June-Skeeter <thruhikerflash@gmail.com>
2025-04-03 15:44:01 -0700

    added links

--- saveDict.py
+++ saveDict.py
@@ -1,14 +1,18 @@
 import os
 import yaml
 import json
+
 # save a dict (obj) to a file (outputPath) in either .json or .yml format
-def saveDict(obj,outputPath,sort_keys=False,indent=None):
+def saveDict(obj,outputPath,sort_keys=False,indent=None,anchors=False):
     if os.path.split(outputPath)[0] == '':
         pass
     elif not os.path.isdir(os.path.split(outputPath)[0]):
         os.makedirs(os.path.split(outputPath)[0])
     with open(outputPath,'w') as file:
         if outputPath.endswith('.yml'):
-            yaml.safe_dump(obj,file,sort_keys=sort_keys)
+            if anchors:
+                yaml.safe_dump(obj,file,sort_keys=sort_keys,default_flow_style=False)
+            else:
+                yaml.safe_dump(obj,file,sort_keys=sort_keys)
         if outputPath.endswith('.json'):
             json.dump(obj,file,indent=indent)
