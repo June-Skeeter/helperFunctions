37c1960eb5653676d059f186286bed352cddab22
June-Skeeter <thruhikerflash@gmail.com>
2026-02-20 16:18:37 -0800

    fixed safeFormat default regex

--- safeFormat.py
+++ safeFormat.py
@@ -10,7 +10,7 @@ def cleanString(stringIn):
 
 
 # Formats string to be safe for a filename (replaces all non alphanumeric characters with underscores by default)
-def safeFormat(stringIn,safeCharacters='[^0-9a-zA-Z/-]+',safeFill='_'):
+def safeFormat(stringIn,safeCharacters='[^0-9a-zA-Z-]+',safeFill='_'):
     stringOut = re.sub(safeCharacters,safeFill, str(stringIn)).rstrip(safeFill).lstrip(safeFill)
     if stringOut == '':
         stringOut = safeFill
